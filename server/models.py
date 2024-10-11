130120d9b95f73d791a87ee913aac23b120052c0
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-11 14:46:24 +0300

    Worked on the validations lab.

--- server/models.py
+++ server/models.py
@@ -11,7 +11,34 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+    @validates('name', 'phone_number')
+    def validates_authors_fields(self, key, value):
+        if key == 'name':
+            author_name = value
+
+            if not author_name or author_name.strip() == '':
+                raise ValueError("Author name is required!")
+            
+            existing_name = Author.query.filter_by(name = author_name).first()
+            if existing_name:
+                raise ValueError("Author name already exists.")
+            
+            return author_name
+        
+        if key == 'phone_number':
+            author_number = value
+
+            compact_number = str(author_number).replace(' ', '')
+
+            if not compact_number.isdigit():
+                raise ValueError("Phone number can only contain digits.")
+
+            if len(compact_number) != 10:
+                raise ValueError("Phone number must be exactly 10 digits.")
+            
+            return compact_number
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +54,51 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('title', 'content', 'category', 'summary')
+    def validates_posts_fields(self, key, value):
+        if key == 'content':
+            post_content = value
 
+            formatted_post_content = str(post_content).replace(' ','')
+            
+            if len(formatted_post_content) < 250:
+                raise ValueError("Content must be at least 250 characters long!")
+            
+            return post_content
+        
+        if key == 'title':
+            post_title = value
+
+            click_bait = ["Won't Believe", "Secret", "Top", "Guess"]
+
+            if not post_title and post_title.strip() == '':
+                raise ValueError("Each post must have a title!")
+            
+            if not any(term in post_title for term in click_bait):
+                raise ValueError(f"Title must have at least one of the following terms: {', '.join(click_bait)}")
+            
+            return post_title
+        
+        if key == 'summary':
+            post_summary = value
+
+            formatted_post_summary = str(post_summary).replace(' ','')
+
+            if len(formatted_post_summary) > 250:
+                raise ValueError("Summary cannot exceed 250 characters!")
+            
+            return post_summary
+        
+        if key == 'category':
+            post_category = value
+
+            valid_categories = ['Fiction', 'Non-Fiction']
+
+            if post_category not in valid_categories:
+                raise ValueError("Category can only be Fiction or Non-Fiction")
+            
+            return post_category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
